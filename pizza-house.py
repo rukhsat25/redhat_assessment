dfebb3cb05773be316c7f64132bedabc573f836f
Utkarsh Gupta <62568180+rukhsat25@users.noreply.github.com>
2022-10-12 20:43:47 +0530

    status code updates

--- pizza-house.py
+++ pizza-house.py
@@ -2,7 +2,7 @@
 
 from crypt import methods
 from flask import Flask
-from flask import jsonify,request
+from flask import jsonify,request,make_response
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -25,8 +25,7 @@ def home():
 #routing app for welcome API
 @app.route('/welcome',methods=['GET'])
 def welcome():
-    resp=jsonify("Welcome to Pizza House")
-    resp.status_code=200
+    resp=jsonify("Welcome to Pizza House","status : 200")
     return resp
 
 #routing app for order API
@@ -37,15 +36,14 @@ def order():
     global order_count
     order_count+=1
     mongo.db.orders.insert_one({'order':_items,"order_id":order_count})
-    resp=jsonify(f"Order ID: {order_count}")
-    resp.status_code=200
+    resp=jsonify(f"Order ID: {order_count}","status : 201")
     return resp
 
 #routing app for getorders API
 @app.route('/getorders',methods=['GET'])
 def get_orders():
     o=mongo.db.orders.find()
-    return dumps(o)
+    return make_response(dumps(o),200)
 
 #routing app for getorders/order_id API
 @app.route('/getorders/<int:order_id>',methods=['GET'])
@@ -54,7 +52,7 @@ def get_orders_by_id(order_id):
     resp=dumps(o)
     if resp==dumps(o):
         return not_found()
-    return resp
+    return make_response(resp,200)
 
 #error_handler
 @app.errorhandler(404)
